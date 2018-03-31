7b3b4dde777a1b1d7d5e70c46936fda64dface3e
Jay Yang <hjyang1@uci.edu>
2018-03-31 13:42:10 -0700

    setting up dna

---
+++ StayOnPlatform/Assets/DNA.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour {
+
+    List<int> genes = new List<int>();
+    int dnaLength = 0;
+    int maxValues = 0;
+
+    public DNA(int l, int v)
+    {
+        dnaLength = l;
+        maxValues = v;
+        SetRandom();
+    }
+
+    public void SetRandom()
+    {
+        genes.Clear();
+        for(int i = 0; i < dnaLength; i++)
+        {
+            genes.Add(Random.Range(0, maxValues));
+            
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
